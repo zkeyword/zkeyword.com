0fb1d14e2e82b0f7b11354da2297dd55d6f649a7
zkeyword <zjx1247225@163.com>
2018-07-06 14:36:34 +0800

    修改样式

--- src/client/router/index.tsx
+++ src/client/router/index.tsx
@@ -1,7 +1,12 @@
 import * as React  from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, withRouter } from 'react-router-dom'
 import * as Loadable from 'react-loadable'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import NotFound from '../containers/NotFound'
+
+interface RouterProps {
+    location: any
+}
 
 const Loading = props => {
     return <div>Loading...</div>
@@ -29,17 +34,30 @@ const About = Loadable({
 
 const isServer = typeof require.ensure !== 'function' // require.ensure webpack 提供
 
-export default class Routes extends React.Component {
+class Routes extends React.Component<RouterProps, any> {
+    constructor(props) {
+        super(props)
+    }
     render() {
+        const { location } = this.props
+        const currentKey = location.pathname.split('/')[1] || '/'
+        const timeout = { enter: 400, exit: 350 }
         return (
-            <Switch>
-                <Route exact path='/' component={isServer ? require('../containers/Home').default : Home} />
-                <Route exact path='/page/:page' component={isServer ? require('../containers/Home').default : Home} />
-                <Route exact path='/post/:name' component={isServer ? require('../containers/Post').default : Post} />
-                <Route exact path='/tag/:name' component={isServer ? require('../containers/Tag').default : Tag} />
-                <Route exact path='/about' component={isServer ? require('../containers/About').default : About} />
-                <Route component={NotFound} />
-            </Switch>
+            <TransitionGroup className='page-main' component='main' id='main' >
+                <CSSTransition key={currentKey} timeout={timeout} classNames='slide' appear>
+                    <Switch>
+                        <Route exact path='/' component={isServer ? require('../containers/Home').default : Home} />
+                        <Route exact path='/page/:page' component={isServer ? require('../containers/Home').default : Home} />
+                        <Route exact path='/post/:name' component={isServer ? require('../containers/Post').default : Post} />
+                        <Route exact path='/tag/:name' component={isServer ? require('../containers/Tag').default : Tag} />
+                        <Route exact path='/about' component={isServer ? require('../containers/About').default : About} />
+                        <Route component={NotFound} />
+                    </Switch>
+                </CSSTransition>
+            </TransitionGroup>
         )
     }
 }
+
+
+export default withRouter(Routes)
