4179d1ec90dcef01270ea1a338b9b61e598c7dfc
zkeyword <zjx1247225@163.com>
2018-08-09 17:44:09 +0800

    添加jest支持ts

--- __test__/component/header.spec.tsx
+++ __test__/component/header.spec.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import * as React from 'react'
 import { shallow } from 'enzyme'
+import { create } from 'react-test-renderer'
+
 import Header from '../../src/client/components/header'
 
 describe('<Header />', () => {
+    // it('Matches snapshot', () => {
+    //     const tree = create(<Header location={'/'} />)
+    //     expect(tree).toMatchSnapshot()
+    // })
 
     it('Clicking the button increments the counter', () => {
         const wrapper = shallow(<Header location={'/'} />)
